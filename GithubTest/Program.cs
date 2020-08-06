89c725d201bba05533a83a2cec1d864a9abadf83
ryanlightbrighton <60697431+ryanlightbrighton@users.noreply.github.com>
2020-08-06 19:21:30 +0100

    Add project files.

---
+++ GithubTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GithubTest {
+    class Program {
+        static void Main(string[] args) {
+            int? val = 3;  // int? is shorthand for Nullable<int>
+            string output;
+            output = val switch
+            {
+                1 => "A",
+                2 => "B",
+                3 => (val * val).ToString(),
+                null => "X",
+                _ => "default case"
+            };
+
+            Console.WriteLine($"output from switch: {output}");
+        }
+    }
+}
